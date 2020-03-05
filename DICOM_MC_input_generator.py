031f42a02fee5887e3ef117082bc9f903a12724b
sangmin <lsm3861@gmail.com>
2020-03-05 14:43:44 +0900

    commit

--- DICOM_MC_input_generator.py
+++ DICOM_MC_input_generator.py
@@ -39,67 +39,117 @@ from scipy.io import FortranFile
 import os
 from subprocess import Popen, PIPE
 
-'''
-# load the DICOM files
-files = []
-print('glob: {}'.format(sys.argv[1]))
-for fname in glob.glob(sys.argv[1], recursive=False):
-    print("loading: {}".format(fname))
-    files.append(pydicom.dcmread(fname))
 
-print("file count: {}".format(len(files)))
+    # # load the DICOM files
+    # files = []
+    # print('glob: {}'.format(sys.argv[1]))
+    # for fname in glob.glob(sys.argv[1], recursive=False):
+    #     print("loading: {}".format(fname))
+    #     files.append(dicom.dcmread(fname))
+    #
+    # print("file count: {}".format(len(files)))
+    #
+    # # skip files with no SliceLocation (eg scout views)
+    # slices = []
+    # skipcount = 0
+    # for f in files:
+    #     if hasattr(f, 'SliceLocation'):
+    #         slices.append(f)
+    #     else:
+    #         skipcount = skipcount + 1
+    #
+    # print("skipped, no SliceLocation: {}".format(skipcount))
+    #
+    # # ensure they are in the correct order
+    # slices = sorted(slices, key=lambda s: s.SliceLocation)
+    #
+    # # pixel aspects, assuming all slices are the same
+    # ps = slices[0].PixelSpacing
+    # ss = slices[0].SliceThickness
+    # ax_aspect = ps[1]/ps[0]
+    # sag_aspect = ps[1]/ss
+    # cor_aspect = ss/ps[0]
+    #
+    # # create 3D array
+    # img_shape = list(slices[0].pixel_array.shape)
+    # img_shape.append(len(slices))
+    # img3d = np.zeros(img_shape)
+    #
+    # # fill 3D array with the images from the files
+    # for i, s in enumerate(slices):
+    #     img2d = s.pixel_array
+    #     img3d[:, :, i] = img2d
+    #
+    # # HU to material index
+    #
+    # # Index based on RS_mask
+    #
+    #
+    # # plot 3 orthogonal slices
+    # a1 = plt.subplot(2, 2, 1)
+    # plt.imshow(img3d[:, :, img_shape[2]//2])
+    # a1.set_aspect(ax_aspect)
+    #
+    # a2 = plt.subplot(2, 2, 2)
+    # plt.imshow(img3d[:, img_shape[1]//2, :])
+    # a2.set_aspect(sag_aspect)
+    #
+    # a3 = plt.subplot(2, 2, 3)
+    # plt.imshow(img3d[img_shape[0]//2, :, :].T)
+    # a3.set_aspect(cor_aspect)
+    #
+    # plt.show()
 
-# skip files with no SliceLocation (eg scout views)
-slices = []
-skipcount = 0
-for f in files:
-    if hasattr(f, 'SliceLocation'):
-        slices.append(f)
-    else:
-        skipcount = skipcount + 1
 
-print("skipped, no SliceLocation: {}".format(skipcount))
+def make3D():
 
-# ensure they are in the correct order
-slices = sorted(slices, key=lambda s: s.SliceLocation)
+    files = []
+    for fname in glob.glob(sys.argv[1], recursive=False):
+        print("loading: {}".format(fname))
+        files.append(dicom.dcmread(fname))
+    print("file count: {}".format(len(files)))
 
-# pixel aspects, assuming all slices are the same
-ps = slices[0].PixelSpacing
-ss = slices[0].SliceThickness
-ax_aspect = ps[1]/ps[0]
-sag_aspect = ps[1]/ss
-cor_aspect = ss/ps[0]
+    # skip files with no SliceLocation (eg scout views)
+    slices = []
+    skipcount = 0
+    for f in files:
+        if hasattr(f, 'SliceLocation'):
+            slices.append(f)
+        else:
+            skipcount = skipcount + 1
+    print("skipped, no SliceLocation: {}".format(skipcount))
 
-# create 3D array
-img_shape = list(slices[0].pixel_array.shape)
-img_shape.append(len(slices))
-img3d = np.zeros(img_shape)
+    # ensure they are in the correct order
+    slices = sorted(slices, key=lambda s: s.SliceLocation)
+    slices.reverse()
 
-# fill 3D array with the images from the files
-for i, s in enumerate(slices):
-    img2d = s.pixel_array
-    img3d[:, :, i] = img2d
+    img3D = np.stack([img2d.pixel_array for img2d in slices], axis=0)
 
-# HU to material index
+    # plt.figure()
+    # for i in range(1, 112):
+    #     plt.subplot(11,11,i)
+    #     plt.title("i = {}".format(i))
+    #     plt.imshow(img3D[i-1], cmap=plt.cm.bone )
+    # plt.show()
+    return img3D
 
-# Index based on RS_mask
+def hu2mcnp_mat():
+    hu3D = make3D()
+
+    # 원본 3D array를 유지하면서, 각각의 material array로 복사 후, 각 material array에 threshold를 이용해서
+    # material index의 정보를 가진 마스크를 만든뒤 모두 합치기
+    material_num = 3
+    #mat1
+    mat = []
+    for i in range(1, material_num):
+        mat[i] =[]
+        mat
+
+    for i in range(1, material_num):
+        mat[[ (10 < mat1) & ( mat1< 100) ] = 1
 
 
-# plot 3 orthogonal slices
-a1 = plt.subplot(2, 2, 1)
-plt.imshow(img3d[:, :, img_shape[2]//2])
-a1.set_aspect(ax_aspect)
-
-a2 = plt.subplot(2, 2, 2)
-plt.imshow(img3d[:, img_shape[1]//2, :])
-a2.set_aspect(sag_aspect)
-
-a3 = plt.subplot(2, 2, 3)
-plt.imshow(img3d[img_shape[0]//2, :, :].T)
-a3.set_aspect(cor_aspect)
-
-plt.show()
-'''
+    #Hounsfield Unit from CT images to Material Index of MCNP
 
 def main():
     # TODO 인풋파일 path 정리하는 함수 필요.
@@ -111,7 +161,6 @@ def main():
     MCNP_input = "MCNP_input"
     MCNP_data = "MCNP_data"
 
-    mcpl
     # Parameters
     pixel_spacing_x = dicom.dcmread(CT_path).PixelSpacing[0]
     pixel_spacing_y = dicom.dcmread(CT_path).PixelSpacing[1]
@@ -138,8 +187,9 @@ def main():
 
     f.close()
 
-    os.sys
-    os.system("/Users/sangmin/GRIDCONV/gridconv")
+    #os.sys
+    #os.system("/Users/sangmin/GRIDCONV/gridconv")
+
     # p = Popen(['/Users/sangmin/GRIDCONV/gridconv'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
     # p.stdout.read()
     #output = p.stdout.read()
